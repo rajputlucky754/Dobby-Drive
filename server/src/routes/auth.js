c2aa4706bc2a46cb77ed6397b96f8aeb5740a51a
Lucky Raja <115029130+rajputlucky754@users.noreply.github.com>
2025-08-13 21:37:44 +0530

    Update auth.js

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -8,8 +8,8 @@ function setTokenCookie(res, id) {
   const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '7d' });
   res.cookie('token', token, {
     httpOnly: true,
-    sameSite: 'lax',
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,      // important for HTTPS
+    sameSite: 'none'   // required for cross-domain
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
 }
